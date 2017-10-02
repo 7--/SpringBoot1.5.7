30280512198a519e7e7384f1e776f215b1a7f874
Dave Syer <dsyer@pivotal.io>
2017-10-02 09:12:14 +0100

    Make it work with Boot 1.3.8

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,15 @@
 package hello;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.*;
-import org.springframework.boot.web.SpringBootServletInitializer;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application  {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
